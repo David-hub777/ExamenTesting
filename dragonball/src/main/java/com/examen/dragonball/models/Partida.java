46dc2a2ce2eb67e17a917a20062aabe4b8ca818a
David <david25042001@gmail.com>
2024-03-11 12:05:39 +0100

    commit

--- dragonball/src/main/java/com/examen/dragonball/models/Partida.java
+++ dragonball/src/main/java/com/examen/dragonball/models/Partida.java
@@ -7,7 +7,8 @@ public class Partida {
     Jugador jugador1;
     Jugador jugador2;
     int ronda;
-    ArrayList<String> winner;
+    String winner;
+    ArrayList<String> players = new ArrayList<String>();
 
     public Jugador getJugador1() {
         return jugador1;
@@ -27,22 +28,33 @@ public class Partida {
     public void setRonda(int ronda) {
         this.ronda = ronda;
     }
-    public ArrayList<String> getWinner() {
+    public String getWinner() {
         return winner;
     }
-    public void setWinner(ArrayList<String> winner) {
-        this.winner = winner;
+    public void setWinner(String ganador) {
+        this.winner = ganador;
     }
-    public void addWinner(String jugador) {
+    public void addWinner(String ganador) {
         ronda++;
-        this.winner.add(jugador);
+        this.winner = ganador;
+        if(ganador.equals("jugador1")){
+            players.add("j1");
+        }else
+            players.add("j2");
+        
     }
 
     
-    public Partida(Jugador jugador1, Jugador jugador2, int ronda, String winner) {
+    public Partida(Jugador jugador1, Jugador jugador2, int ronda) {
         this.jugador1 = jugador1;
         this.jugador2 = jugador2;
         this.ronda = ronda;
-        this.winner.add(winner) ;
+        // this.winner.add(winner) ;
+    }
+    public ArrayList<String> getPlayers() {
+        return players;
+    }
+    public void setPlayers(ArrayList<String> players) {
+        this.players = players;
     }
 }
--- dragonball/src/main/java/com/examen/dragonball/restController/JanKenPonController.java
+++ dragonball/src/main/java/com/examen/dragonball/restController/JanKenPonController.java
@@ -1,27 +1,62 @@
 package com.examen.dragonball.restController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.examen.dragonball.models.Dragonball;
+import com.examen.dragonball.models.Jugador;
+import com.examen.dragonball.models.Partida;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import com.examen.dragonball.models.DragonballOwn;
 
 @RestController
 @RequestMapping(path = "/jankenpon")
 public class JanKenPonController {
+    
+    Jugador jugador1 = new Jugador("Jugador1", "");
+    Jugador jugador2 = new Jugador("Jugador2", "");
+    Partida partida = new Partida(jugador1,jugador2,0);
+    String[]elecciones = {"piedra","papel","tijeras"};
+    
+
+    @PostMapping(path="/play", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Partida> play(@RequestParam String jugador1, @RequestParam String jugador2) {
+        String winner ="";
+        System.out.println(jugador1);
+        if(jugador1.equals(elecciones[0]) && jugador2.equals(elecciones[2])){
+            winner = "jugador1";
+            partida.addWinner(winner);
+            return new ResponseEntity<Partida>(partida, HttpStatus.ACCEPTED);
+        }else if(jugador1.equals(elecciones[1]) && jugador2.equals(elecciones[0])){
+            winner = "jugador1";
+            partida.addWinner(winner);
+            
+            return new ResponseEntity<Partida>(partida, HttpStatus.ACCEPTED);
+        }else if(jugador1.equals(elecciones[2]) && jugador2.equals(elecciones[1])){
+            winner = "jugador1";
+            partida.addWinner(winner);
+            return new ResponseEntity<Partida>(partida, HttpStatus.ACCEPTED);
+        }else if(jugador1.equals(jugador2)){
+            winner = "Empate";
+            partida.addWinner(winner);
+            return new ResponseEntity<Partida>(partida, HttpStatus.ACCEPTED);
+        }else{
+            winner = "jugador2";
+            partida.addWinner(winner);
+            return new ResponseEntity<Partida>(partida, HttpStatus.ACCEPTED);
+        }
+		
+		
+	}
+
+    @GetMapping(path="/results/{jugador}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String results(@PathVariable String jugador) {
+        return new String();
+    }
     
 }
